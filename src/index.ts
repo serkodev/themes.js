15d3cb57e536acf9871c1742efe8f30349b20128
SerKo <serko.dev@gmail.com>
2022-05-20 00:10:18 +0800

    feat: add media listener

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,26 @@
-console.log('hello world')
+const MEDIA = '(prefers-color-scheme: dark)'
+
+class Theme {
+  constructor() {
+    const media = window.matchMedia(MEDIA)
+    try {
+      media.addEventListener('change', this.handleMediaQuery)
+    }
+    catch (e1) {
+      try {
+        // Safari
+        media.addListener(this.handleMediaQuery)
+      }
+      catch (e2) {
+        console.error(e2)
+      }
+    }
+    this.handleMediaQuery(media)
+  }
+
+  handleMediaQuery(e: MediaQueryListEvent | MediaQueryList) {
+    console.log('media', e)
+  }
+}
+
+export default Theme
