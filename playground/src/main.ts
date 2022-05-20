b40c6ef24f6b305d62bd9f3b72e51cf3c31a2699
SerKo <serko.dev@gmail.com>
2022-05-20 19:24:13 +0800

    feat: update playground for testing

--- playground/src/main.ts
+++ playground/src/main.ts
@@ -1,12 +1,14 @@
 import './style.css'
 import Themes from '../../src/index'
 
-const app = document.querySelector<HTMLDivElement>('#app')!
-
-app.innerHTML = `
-  <h1>Hello Vite!</h1>
-  <a href="https://vitejs.dev/guide/features.html" target="_blank">Documentation</a>
-`
-
 // eslint-disable-next-line no-new
-new Themes()
+const themes = new Themes()
+
+const handleThemeSelected = (e: Event) => {
+  const theme = (e.target as HTMLInputElement).value
+  themes.setTheme(theme)
+}
+
+document.querySelectorAll('input[name=\'theme\']').forEach((input) => {
+  input.addEventListener('change', handleThemeSelected)
+})
