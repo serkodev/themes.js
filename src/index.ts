4efe1352c87211472ef42d20c0970c9449b748be
SerKo <serko.dev@gmail.com>
2022-05-20 19:24:00 +0800

    feat: observe media and apply theme

--- src/index.ts
+++ src/index.ts
@@ -1,25 +1,78 @@
 const MEDIA = '(prefers-color-scheme: dark)'
 
+const getSystemTheme = (e?: MediaQueryList | MediaQueryListEvent) => {
+  if (!e)
+    e = window.matchMedia(MEDIA)
+
+  const isDark = e.matches
+  const systemTheme = isDark ? 'dark' : 'light'
+  return systemTheme
+}
+
 class Theme {
-  constructor() {
+  removeListenerFunc?: Function
+  currentTheme = ''
+
+  constructor(theme = 'auto') {
+    this.setTheme(theme)
+  }
+
+  private observe() {
+    if (this.removeListenerFunc !== undefined)
+      return
+
     const media = window.matchMedia(MEDIA)
+    const handler = this.handleMediaQuery.bind(this)
     try {
-      media.addEventListener('change', this.handleMediaQuery)
-    }
-    catch (e1) {
+      media.addEventListener('change', handler)
+      this.removeListenerFunc = () => {
+        media.removeEventListener('change', handler)
+      }
+    } catch (e1) {
       try {
         // Safari
-        media.addListener(this.handleMediaQuery)
-      }
-      catch (e2) {
+        media.addListener(handler)
+        this.removeListenerFunc = () => {
+          media.removeListener(handler)
+        }
+      } catch (e2) {
         console.error(e2)
       }
     }
     this.handleMediaQuery(media)
   }
 
-  handleMediaQuery(e: MediaQueryListEvent | MediaQueryList) {
-    console.log('media', e)
+  private stopObserve() {
+    if (this.removeListenerFunc !== undefined) {
+      this.removeListenerFunc()
+      delete this.removeListenerFunc
+    }
+  }
+
+  private handleMediaQuery(e: MediaQueryListEvent | MediaQueryList) {
+    const theme = getSystemTheme(e)
+    this.applyTheme(theme)
+  }
+
+  private applyTheme(theme: string) {
+    if (theme === this.currentTheme)
+      return
+
+    const e = document.documentElement
+    if (e.classList.contains(this.currentTheme)) {
+      e.classList.remove(this.currentTheme)
+    }
+    e.classList.add(theme)
+    this.currentTheme = theme
+  }
+
+  setTheme(theme: string) {
+    if (theme === 'auto') {
+      this.observe()
+    } else {
+      this.stopObserve()
+      this.applyTheme(theme)
+    }
   }
 }
 
